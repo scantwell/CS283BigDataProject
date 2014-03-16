17542161e9eb9ac025336ba05340dee4a9708d51
Michael Brisbin <brisbin-pc@Brisbin-PC.(none)>
2014-03-16 03:22:12 -0400

    Socket using openssl - couldn't get lib to work after installing or on tux

--- src/connection.c
+++ src/connection.c
@@ -5,12 +5,14 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <netdb.h>
-
+#include <openssl/ssl.h>
 int main()
 {
-
-    char * hostname = "www.reddit.com";
-    int         port        = 80;
+    SSL_load_error_strings ();
+    SSL_library_init ();
+    SSL_CTX *ssl_ctx = SSL_CTX_new (SSLv23_client_method ());
+    char * hostname = "cloudmine.me";
+    int         port        = 443;
     int         s,rc;
     //struct sockaddr_in name;
     struct sockaddr_in serv_addr, cli_addr;
@@ -41,21 +43,22 @@ int main()
         perror("could not find host");
         return 0;
     }
-    
+      
     memcpy((void *)&serv_addr.sin_addr, hp->h_addr_list[0], hp->h_length);
-
+    SSL *conn = SSL_new(ssl_ctx);
+    SSL_set_fd(conn, s);
     if ( connect(s, (struct sockaddr *)&serv_addr, sizeof(serv_addr) )){
         perror("connect failed");
         return 0;
     }
-    
-    if ( (send( s, "GET / HTTP/1.1\r\n\r\n", 22, 0 )) != 22 ){
+    char *header ="GET /v1/app/8b98552e5ad6425283215ea4d4339f7d/text? HTTP/1.1 \r\nHost:cloudmine.me \r\nX-CloudMine-ApiKey: 4a1bbce6b8864246a52262fe920dad52\r\n\r\n"; 
+    if ( (SSL_write( s,header, strlen(header)+1, 0 )) != 22 ){
         printf("Did not send everything");
     }
     char buffer[1024];
     int len = 1023;
-    len = read(s, buffer, len);
-    printf("THIS IS BUFF %s\n", buffer);
+    len = SSL_read(s, buffer, len);
+    printf("%s\n", buffer);
     
    // close(s);
     
